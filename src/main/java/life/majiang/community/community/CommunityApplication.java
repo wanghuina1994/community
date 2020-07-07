fa93525c5de06d8dd5b3c495e4759a4a6c570a38
wanghuina <18810129962@163.com>
2020-07-07 17:11:23 +0800

    insert data

--- src/main/java/life/majiang/community/community/CommunityApplication.java
+++ src/main/java/life/majiang/community/community/CommunityApplication.java
@@ -2,8 +2,11 @@ package life.majiang.community.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"life.majiang.community.community.mapper"})
 @SpringBootApplication
+
 public class CommunityApplication {
 
     public static void main(String[] args) {
--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -3,6 +3,8 @@ package life.majiang.community.community.controller;
 import life.majiang.community.community.Provider.GitHubProvider;
 import life.majiang.community.community.dto.AccessTokenDTO;
 import life.majiang.community.community.dto.GithubUser;
+import life.majiang.community.community.mapper.UserMapper;
+import life.majiang.community.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.print.attribute.standard.PresentationDirection;
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
+    @Autowired
+    private UserMapper userMapper;
     @Autowired
     private GitHubProvider GitHubProvider;
     //常量配置在文件中
@@ -23,10 +28,13 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.client.uri}")
     private String clientUri;
+
     @GetMapping("/callback")
+
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
                            HttpServletRequest request){
+        System.out.println("111122");
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -34,10 +42,20 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(clientUri);
         accessTokenDTO.setState(state);
         String accessToken = GitHubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = GitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        if (user != null) {
-            request.getSession().setAttribute("user",user);
+        GithubUser githubUser = GitHubProvider.getUser(accessToken);
+        System.out.println("1111");
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            System.out.println("main方法==========执行开始2");
+            userMapper.insert(user);
+            System.out.println("main方法==========执行开始3");
+            //存入session
+            request.getSession().setAttribute("user",githubUser);
         }
         return "redirect:/";
 
